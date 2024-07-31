4fdb70426ff2f60c929d65514b7978409dab33f8
Bikramjit Singh <bikramjiit.singh@utoronto.ca>
2024-07-31 16:02:09 -0400

    Update bash script

--- run_baggit_export_script.sh
+++ run_baggit_export_script.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 set -e
 
-# Parameters Passed
-BUILD=$1
-UPLOAD_FILEPATH=$2
-CONF_FILE=$3
+# Initialize variables
+BUILD=""
+UPLOAD_FILEPATH=""
+CONF_FILE=""
+HELP=""
 
-if [ -z "$BUILD" ] || [ -z "$UPLOAD_FILEPATH" ]
-then
-  echo "Error: Both BUILD and UPLOAD_FILEPATH parameters are required"
-  exit 1
+# Function to display help
+display_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -b, --build_number BUILD_NUMBER     Specify the build number"
+    echo "  -f, --upload-file UPLOAD_FILEPATH   Specify the upload file path"
+    echo "  -c, --config-file CONF_FILE       Specify the config file path (optional)"
+    echo "  -h, --help                   Display this help message"
+    exit 1
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -b|--build_number)
+            BUILD="$2"
+            shift 2
+            ;;
+        -f|--upload-file)
+            UPLOAD_FILEPATH="$2"
+            shift 2
+            ;;
+        -c|--config-file)
+            CONF_FILE="$2"
+            shift 2
+            ;;
+        -h|--help)
+            HELP="true"
+            shift
+            ;;
+        *)
+            echo "Error: Invalid option: $1"
+            display_help
+            ;;
+    esac
+done
+
+# Display help if requested
+if [ -n "$HELP" ]; then
+    display_help
+fi
+
+# Check if required parameters are provided
+if [ -z "$BUILD" ] || [ -z "$UPLOAD_FILEPATH" ]; then
+    echo "Error: Both BUILD and UPLOAD_FILEPATH parameters are required"
+    display_help
 fi
 
 if [ -n "$CONF_FILE" ]; then
@@ -26,27 +69,23 @@ APP_FOLDER=$(dirname "$SCRIPT")
 VENV_PATH="venv"
 
 echo " ** Starting baggit export script with following parameters **"
-
 echo "Build Number : $BUILD"
 echo "App Path : $APP_FOLDER"
 echo "Upload file path : $UPLOAD_FILEPATH"
-#echo "Updating following file from uploaed CSV file | fileId,Location|"
-#cat $UPLOAD_FILEPATH
 echo -e "\n\n"
 
-cd $APP_FOLDER || exit 1
-echo "Creating/Updating  python virtual environment"
-/bin/python3 -m venv $VENV_PATH || exit 1
-./$VENV_PATH/bin/pip3 install -r requirements.txt || exit 1
-if [ $? = 1 ]
-then
-  exit 1
+cd "$APP_FOLDER" || exit 1
+echo "Creating/Updating python virtual environment"
+/bin/python3 -m venv "$VENV_PATH" || exit 1
+./"$VENV_PATH"/bin/pip3 install -r requirements.txt || exit 1
+if [ $? = 1 ]; then
+    exit 1
 fi
 
-./$VENV_PATH/bin/python3 -u $APP_FOLDER/main.py $CONFIG_OPTION  -b $BUILD $UPLOAD_FILEPATH || exit 1
-if [ $? -ne 0 ]
-then
-  exit 1
+./"$VENV_PATH"/bin/python3 -u "$APP_FOLDER"/main.py $CONFIG_OPTION -b "$BUILD" "$UPLOAD_FILEPATH" || exit 1
+if [ $? -ne 0 ]; then
+    exit 1
 fi
+
 echo "<<<< RUN SUCCESSFULLY COMPLETED! >>>>"
 exit 0
